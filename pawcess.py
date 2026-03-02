95e875d97e3ec1a824eb60371370ac20ea9d00ce
gh-commits <hello@istiak.com>
2026-03-02 12:23:20 +0600

    Create pawcess.py

---
+++ pawcess.py
@@ -0,0 +1,24 @@
+import json
+import sys
+
+fp = sys.argv[1]
+
+with open(fp, 'r') as file:
+    for line in file:
+        jsonobj = json.loads(line)
+        # print(jsonobj)
+        # print(jsonobj['data'])
+        print(jsonobj['company_number'])
+        print(jsonobj['data']['address'])
+        print(jsonobj['data']['etag'])
+        print(jsonobj['data']['kind'])
+        print(jsonobj['data']['name'])
+        print(jsonobj['data']['natures_of_control'])
+        print(jsonobj['data']['notified_on'])
+        print(jsonobj['data']['links'])
+
+        if jsonobj['data']['kind'] != "corporate-entity-person-with-significant-control":
+            print(jsonobj['data']['country_of_residence'])
+            print(jsonobj['data']['date_of_birth'])
+            print(jsonobj['data']['nationality'])
+            print(jsonobj['data']['name_elements'])
